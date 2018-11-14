2fc4f5dcfa374f78000f6c2159600f4ccafc09f2
ece <eceercan91@hotmail.com>
2018-11-14 15:26:27 +0300

    Add project files.

---
+++ CompanyService/CompanyService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CompanyService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CompanyService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CompanyService.svc or CompanyService.svc.cs at the Solution Explorer and start debugging.
+    public class CompanyService : ICompanyPublicService, ICompanyConfidentialService
+    {
+        public string GetConfidentialInformation()
+        {
+            return "This is confidential info and only available over TCP behind the company firewall";
+        }
+
+        public string GetPublicInformation()
+        {
+            return "This is public information and available over HTTP to all general public outside the firewall";
+        }
+    }
+}
---
+++ CompanyService/ICompanyService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CompanyService
+{
+    [ServiceContract]
+    public interface ICompanyPublicService
+    {
+        //Bu methoda herkes erişebilir olacak, yani HTTP üzerinden Firewall'a takılmadan erişim
+        [OperationContract]
+        string GetPublicInformation();
+    }
+
+
+    [ServiceContract]
+    public interface ICompanyConfidentialService
+    {
+        //Bu methoda sadece, firewall üzerinden, employeeler erişebilir olacak, o nedenle bu TCP protokolü ile erişilebilicek
+        [OperationContract]
+        string GetConfidentialInformation();
+    }
+}
